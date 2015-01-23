5664eb96fa7844a00214191bb0afed7557ecb84f
Yuriy Hvozdovych <gvizdyk@quintagroup.com>
2015-01-23 13:30:45 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ App.config([
     }
 ]);
 
-App.controller("NewsListM", function($scope, $http) {
+App.controller("NewsListM", function($scope, $http, $filter) {
     $scope.showData = function() {
         $scope.curPage = 0;
         $scope.pageSize = 9;
@@ -14,6 +14,14 @@ App.controller("NewsListM", function($scope, $http) {
             $scope.newslist = res.data;
         });
         $scope.numberOfPages = function() {
+            return Math.ceil($scope.newslist.length / $scope.pageSize);
+        };
+        $scope.filterWIthSearch = function() {
+            var items = $filter('filter')($scope.newslist, search.categories);
+            items = $filter('pagination')(items, $scope.curPage*$scope.pageSize);
+            items = $filter('limitTo')(items, pageSize);
+            
+            $scope.numberOfPages = Math.ceil(items.length / $scope.pageSize);
             return Math.ceil($scope.newslist.length / $scope.pageSize);
         };
     };
