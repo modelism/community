00717e0cabdef05ea720b43a2455fa6e5d33f855
Yuriy Hvozdovych <gvizdyk@quintagroup.com>
2015-02-16 17:35:29 +0000

    updated create_itemnews_json.py

--- json/create_itemnews_json.py
+++ json/create_itemnews_json.py
@@ -3,26 +3,56 @@
 import requests
 import json
 from lxml import html
+from datetime import datetime
+
+now_date = datetime.now()
 
 with open('newsmodelism.json', 'r') as fp:
     items = json.load(fp)
 
-# url = 'http://blog.oscarliang.net/make-simple-whip-antenna-5-8ghz-linear-polo/'
 url = raw_input("Enter a link: ")
 
 r = requests.get(url)
 tree = html.fromstring(r.text)
 
-pub = tree.xpath("//meta[@property='article:published_time']/@content")
-tit = tree.xpath('//header[contains(@class, "entry-header")]/h1/text()')
-paragrafs = tree.xpath('//div[contains(@class, "e-content")]/p')
-images = tree.xpath('//div[contains(@class, "e-content")]//img')
+n = url[0:20]
+print n
+
+if n == "http://blog.oscarlia":
+	print "Site: http://blog.oscarliang.net \n"
+	var_pub = "//meta[@property='article:published_time']/@content"
+	var_tit = "//header[contains(@class, 'entry-header')]/h1/text()"
+	var_paragrafs = "//div[contains(@class, 'e-content')]/p"
+	var_images = "//div[contains(@class, 'e-content')]//img"
+elif n == "http://helipilotonli":
+	print "Site: http://helipilotonline.com \n"
+	print "Today is:", now_date.isoformat()
+	var_pub = raw_input("Enter a date: ")
+	var_tit = "//*[@id='main-content']/div[contains(@class, 'content')]/article//h1/span/text()"
+	var_paragrafs = "//*[@id='main-content']/div[contains(@class, 'content')]/article//div[contains(@class, 'entry')]/p"
+	var_images = "//*[@id='main-content']/div[contains(@class, 'content')]/article//div[contains(@class, 'entry')]//img"
+else:
+   print "Error"
+
+
+if n != "http://helipilotonli":
+	pub = ''.join(tree.xpath(var_pub))
+else:
+	pub = var_pub
+
+tit = tree.xpath(var_tit)
+paragrafs = tree.xpath(var_paragrafs)
+images = tree.xpath(var_images)
+
+print "Categories: 'Aircraftflite', 'Helicopters', 'Multicopters', 'Parts', 'Video'"
+cat = input("Enter a categories: ")
+
+print cat
 
 def lp(pp):
 	for i in pp:
 		p = i.xpath('text()|*/text()|*/*/text()|*/*/*/text()|*/*/*/*/text()')
 		if len(p) > 0:
-			# print p
 			return p
 
 def li(ii):
@@ -34,9 +64,9 @@ item = {
 	'title': ''.join(tit),
 	'img': ''.join(li(images)),
 	'description': ''.join(lp(paragrafs)),
-	'published': ''.join(pub),
+	'published': pub,
     'url':url,
-    'categories':'Multicopters',
+    'categories': cat,
     'domain':''
 }
 
