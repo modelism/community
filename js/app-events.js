2fd9612590511d42f827b811a13994e5aa60470c
Yuriy Hvozdovych <gvizdyk@quintagroup.com>
2015-03-10 00:35:12 +0200

    added page events

---
+++ js/app-events.js
@@ -0,0 +1,39 @@
+var App = angular.module("App", []);
+
+App.config([
+    '$interpolateProvider', function($interpolateProvider) {
+        return $interpolateProvider.startSymbol('{(').endSymbol(')}');
+    }
+]);
+
+App.controller("EventsListM", function($scope, $http, $filter) {
+    $scope.showData = function() {
+        $scope.curPage = 0;
+        $scope.pageSize = 9;
+        $scope.eventslist = [];
+        $http.get('/json/events.json').then(function(res){
+            $scope.eventslist = res.data;
+        });
+        $scope.numberOfPages = function() {
+            return Math.ceil($scope.eventslist.length / $scope.pageSize);
+        };
+        $scope.filterWIthSearch = function() {
+            var items = $filter('filter')($scope.eventslist, ($scope.search||{}).categories||"");
+            $scope.numberOfPages = Math.ceil(items.length / $scope.pageSize);
+            if ($scope.numberOfPages<($scope.curPage+1)){
+                $scope.curPage = 0;
+            };
+            items = $filter('pagination')(items, $scope.curPage*$scope.pageSize);
+            items = $filter('orderBy')(items, '-published');
+            items = $filter('limitTo')(items, $scope.pageSize);
+            return items;
+        };
+    };
+});
+
+angular.module("App").filter('pagination', function() {
+    return function(input, start) {
+        start = +start;
+        return input.slice(start);
+    };
+});
